669ad434d3941957c486b8f8a87d1bbcca3adcf1
Chihiro Ura <chiichii19991104@live.com>
2023-01-15 14:00:31 +0900

    [fix] calc rt

--- visualize.py
+++ visualize.py
@@ -6,21 +6,29 @@ import utils
 import stereoSGM
 
 
-def run(d_img):
+def run(d_img, color=None):
     d_img = cv2.rotate(d_img, cv2.ROTATE_90_CLOCKWISE)
+    color = cv2.rotate(color, cv2.ROTATE_90_CLOCKWISE)
+    color = cv2.resize(color, [d_img.shape[1], d_img.shape[0]])
     cv2.imshow("show", d_img)
     cv2.waitKey(0)
+    cv2.imshow("show1", color)
+    cv2.waitKey(0)
+
     pcd = open3d.geometry.PointCloud()
+    color = cv2.cvtColor(color, cv2.COLOR_BGR2RGB)
 
     for i in range(d_img.shape[0]):
         for j in range(d_img.shape[1]):
             ni = (i - (d_img.shape[0] / 2)) / d_img.shape[0] * 2
             ji = (j - (d_img.shape[1] / 2)) / d_img.shape[1] * 2
-            pt = utils.equi_to_xyz([ji, ni])
-            pt = pt * ((1 - d_img[i, j]) + 0.5) * 3
+            vec = utils.equi_to_xyz([ji, ni])
+            norm = (1 - d_img[i, j])
+            pt = vec * (norm + (np.cos(ni)))**2
 
             if not d_img[i, j] <= 0.1:
-                pcd.points.append(pt)
+                pcd.points.append([pt[0], pt[1], pt[2]])
+                pcd.colors.append(color[i, j, :] / 255.0)
 
     open3d.visualization.draw_geometries(
         [pcd],
@@ -31,4 +39,5 @@ def run(d_img):
 
 
 if __name__ == "__main__":
-    run(np.load("parallax.npy"))
+    color_img = cv2.imread("tmp/lower_tmp.png")
+    run(np.load("parallax.npy"), color_img)
